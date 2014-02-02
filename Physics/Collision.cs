ebc35183fdd280a6fc93829b752708d5abc995d4
Matt English <tcbjmdme@yahoo.com>
2014-02-01 19:03:08 -0800

    Including the Physics files missed in the previous commit.

---
+++ Physics/Collision.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace LightningBug
+{
+    public class Collision
+    {
+        LightningBug homeClass;
+        private static Collision collision;
+
+        private Collision(LightningBug main)
+        {
+            homeClass = main;
+        }
+
+        public static Collision Instance(LightningBug main)
+        {
+            if (collision == null)
+                collision = new Collision(main);
+
+            return collision;
+        }
+
+        // Checks the given ship against the edges of the level and all colliding objects
+        public void CheckShip(Ship toCheck)
+        {
+            Ship playerShip = homeClass.GetPlayersShip();
+            if (playerShip.GetId() != playerShip.GetId())
+            {
+                //RectRect(toCheck.GetPosition(), toCheck.GetSize(), playerShip.GetPosition(), playerShip.GetSize());
+                CheckIntersection(toCheck.collisionPolygons, playerShip.collisionPolygons);
+            }
+            //enemyShips
+
+            // Lastly check against the level
+            Level curLevel = homeClass.GetCurLevel();
+
+        }
+
+        private bool CheckIntersection(List<Physics.Polygon> objectPolys1, List<Physics.Polygon> objectPolys2)
+        {
+            Vector2 curPoint, curNormal;
+            foreach(Physics.Polygon poly1 in objectPolys1)
+            {
+                foreach(Physics.Polygon poly2 in objectPolys2)
+                {
+                    int i, j;
+                    for (i = poly1.numVertices - 1, j = 0; j < poly1.numVertices; i = j++)
+                    {
+                        curPoint = poly1.vertices[j];
+                        curNormal = poly1.normals[i];
+                        if (WhichSide(poly2, curPoint, curNormal) > 0)
+                            return false;
+                    }
+
+                    for (i = poly2.numVertices - 1, j = 0; j < poly2.numVertices; i = j++)
+                    {
+                        curPoint = poly2.vertices[j];
+                        curNormal = poly2.normals[i];
+                        if (WhichSide(poly1, curPoint, curNormal) > 0)
+                            return false;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private int WhichSide(Physics.Polygon poly, Vector2 point, Vector2 normal)
+        {
+            int posCount, negCount, zeroCount;
+            posCount = negCount = zeroCount = 0;
+            for (int i = 0; i < poly.numVertices; ++i)
+            {
+                float time = Vector2.Dot(normal, poly.vertices[i] - point);
+                if (time > 0)
+                    posCount++;
+                else if (time < 0)
+                    negCount++;
+                else
+                    zeroCount++;
+                if ((posCount > 0 && negCount > 0) || zeroCount > 0)
+                    return 0;
+            }
+
+            if (posCount > 0)
+                return 1;
+            else
+                return -1;
+        }
+
+        /*
+        public void RectRect(Vector2 rect1Pos, Vector2 rect1Size, Vector2 rect2Pos, Vector2 rect2Size)
+        {
+            if (rect2Pos.X > rect1Pos.X && rect2Pos.X < (rect1Pos.X + rect1Size.X))
+            {
+            }
+        }*/
+    }
+}
---
+++ Physics/Polygon.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace LightningBug.Physics
+{
+    public class Polygon
+    {
+        public int numVertices;
+        public Vector2[] vertexOffests; // CCW oriented.  Offset for each vertex from the objects new position.  Created in the ship editor
+        public Vector2[] nonRotatedVertices; // CCW oriented.  These are used to calculate the rotated vertice every frame
+        public Vector2[] vertices; // CCW oriented
+        public Vector2[] normals; // vertices[i] = v[i+1] - v[i]
+
+        public Polygon(Vector2 newPos, Vector2[] offests, int numberOfVertices)
+        {
+            numVertices = numberOfVertices;
+            vertexOffests = new Vector2[numVertices];
+            nonRotatedVertices = new Vector2[numVertices];
+            vertices = new Vector2[numVertices];
+            normals = new Vector2[numVertices];
+            if (offests.Length != numVertices)
+            {
+                Logging.Instance(Logging.DEFAULTLOG).Log("Shapes::Shapes(Vector2 newPos, Vector2[] offests, int numberOfVertices): offests.Length != numVertices\n");
+                System.Environment.Exit(0);
+            }
+            for (int i = 0; i < numVertices; ++i)
+            {
+                vertexOffests[i] = offests[i];
+            }
+        }
+        public void SetPosition(Vector2 newPos, Point center, float radAngle)
+        {
+            //if we define dx=x2-x1 and dy=y2-y1, then the normals are (-dy, dx) and (dy, -dx).
+            for (int i = 0; i < numVertices; ++i)
+            {
+                nonRotatedVertices[i] = newPos + vertexOffests[i];
+
+                double cosAngle = Math.Cos(radAngle);
+                double sinAngle = Math.Sin(radAngle);
+                double dx = (newPos.X - center.X);
+                double dy = (newPos.Y - center.Y);
+
+                vertices[i].X = center.X + (int)(dx * cosAngle - dy * sinAngle);
+                vertices[i].Y = center.Y + (int)(dx * sinAngle + dy * cosAngle);
+                float nx = 0, ny = 0;
+                if (i < numVertices - 1)
+                {
+                    nx = vertices[i+1].X - vertices[i].X;
+                    ny = vertices[i+1].Y - vertices[i].Y;
+                }
+                else if (i == numVertices - 1)
+                {
+                    nx = vertices[0].X - vertices[i].X;
+                    ny = vertices[0].Y - vertices[i].Y;
+                }
+                else
+                    Logging.Instance(Logging.DEFAULTLOG).Log("Shapes::SetPosition(): Index >= numVertices\n");
+                normals[i].X = -ny;
+                normals[i].Y = nx;
+            }
+        }
+    }
+}
