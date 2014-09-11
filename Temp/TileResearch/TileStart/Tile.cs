7cef108d0f67c5079196cec599a57f0fe94829cf
Matt English <tcbjmdme@yahoo.com>
2014-09-10 18:15:51 -0700

    Temp Sample Copy

---
+++ Temp/TileResearch/TileStart/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TileStart
+{
+    static class Tile
+    {
+        static public Texture2D TileSetTexture;
+        static public int TileWidth = 64;
+        static public int TileHeight = 64;
+        static public int TileStepX = 64;
+        static public int TileStepY = 16;
+        static public int OddRowXOffset = 32;
+        static public int HeightTileOffset = 32;
+
+        static public Rectangle GetSourceRectangle(int tileIndex)
+        {
+            int numInRow = TileSetTexture.Width / TileWidth;
+            return new Rectangle((tileIndex % numInRow) * TileWidth, (tileIndex / numInRow) * TileHeight,
+                TileWidth, TileHeight);
+        }
+    }
+}
